b2615e4a23ea088941053d638cb94da0b5277dc6
Jonathan Li <jonathanli668899@gmail.com>
2025-01-22 08:27:19 -0500

    loggeddashboardnumber deprecated tho

--- src/main/java/frc/robot/utils/TunableNumber.java
+++ src/main/java/frc/robot/utils/TunableNumber.java
@@ -1,87 +1,86 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
 package frc.robot.utils;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.DoubleSupplier;
 import frc.robot.Constants;
+import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
 
 /**
  * Class for a tunable number. Gets value from dashboard in tuning mode, returns default if not or
  * value not in dashboard.
  */
-public class TunableNumber {
+public class TunableNumber implements DoubleSupplier {
   private static final String tableKey = "TunableNumbers";
 
-  private String key;
+  private final String key;
+  private boolean hasDefault = false;
   private double defaultValue;
-  private double lastHasChangedValue = defaultValue;
+  private LoggedDashboardNumber dashboardNumber; //deprecated?
+  private Map<Integer, Double> lastHasChangedValues = new HashMap<>();
 
-  /**
-   * Create a new TunableNumber
-   * 
-   * @param dashboardKey Key on dashboard
-   */
   public TunableNumber(String dashboardKey) {
     this.key = tableKey + "/" + dashboardKey;
   }
 
-  /**
-   * Create a new TunableNumber with the default value
-   * 
-   * @param dashboardKey Key on dashboard
-   * @param defaultValue Default value
-   */
+
   public TunableNumber(String dashboardKey, double defaultValue) {
     this(dashboardKey);
-    setDefault(defaultValue);
+    initDefault(defaultValue);
   }
 
-  /**
-   * Get the default value for the number that has been set
-   * 
-   * @return The default value
-   */
-  public double getDefault() {
-    return defaultValue;
-  }
-
-  /**
-   * Set the default value of the number
-   * 
-   * @param defaultValue The default value
-   */
-  public void setDefault(double defaultValue) {
-    this.defaultValue = defaultValue;
-    if (Constants.tuningMode) {
-      // This makes sure the data is on NetworkTables but will not change it
-      SmartDashboard.putNumber(key,
-          SmartDashboard.getNumber(key, defaultValue));
-    } else {
-      SmartDashboard.clearPersistent(key);
+  //Set the default value of the number. The default value can only be set once.
+  public void initDefault(double defaultValue) {
+    if (!hasDefault) {
+      hasDefault = true;
+      this.defaultValue = defaultValue;
+      if (Constants.tuningMode) {
+        dashboardNumber = new LoggedDashboardNumber(key, defaultValue);
+      }
     }
   }
 
-  /**
-   * Get the current value, from dashboard if available and in tuning mode
-   * 
-   * @return The current value
-   */
+  //Get the current value, from dashboard if available and in tuning mode.
   public double get() {
-    return Constants.tuningMode ? SmartDashboard.getNumber(key, defaultValue)
-        : defaultValue;
+    if (!hasDefault) {
+      return 0.0;
+    } else {
+      return Constants.tuningMode ? dashboardNumber.get() : defaultValue;
+    }
   }
 
-  /**
-   * Checks whether the number has changed since our last check
-   * 
-   * @return True if the number has changed since the last time this method was called, false
-   *         otherwise
-   */
-  public boolean hasChanged() {
+  public boolean hasChanged(int id) {
     double currentValue = get();
-    if (currentValue != lastHasChangedValue) {
-      lastHasChangedValue = currentValue;
+    Double lastValue = lastHasChangedValues.get(id);
+    if (lastValue == null || currentValue != lastValue) {
+      lastHasChangedValues.put(id, currentValue);
       return true;
     }
 
     return false;
+  }
+
+  public static void ifChanged(
+      int id, Consumer<double[]> action, TunableNumber... tunableNumbers) {
+    if (Arrays.stream(tunableNumbers).anyMatch(tunableNumber -> tunableNumber.hasChanged(id))) {
+      action.accept(Arrays.stream(tunableNumbers).mapToDouble(TunableNumber::get).toArray());
+    }
+  }
+
+  //Runs action if any of the tunableNumbers have changed
+  public static void ifChanged(int id, Runnable action, TunableNumber... tunableNumbers) {
+    ifChanged(id, values -> action.run(), tunableNumbers);
+  }
+
+  @Override
+  public double getAsDouble() {
+    return get();
   }
 }
