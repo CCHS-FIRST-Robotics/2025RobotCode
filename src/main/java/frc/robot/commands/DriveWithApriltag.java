68ddcdc6086ab1fd38325ce0cc714094defaa7d1
willmell <whickey27@concordcarlisle.org>
2025-03-22 22:05:04 -0400

    added pid control and sepretly added homing to middle of tag then going to a rotation (all my stuff is commented out))

--- src/main/java/frc/robot/commands/DriveWithApriltag.java
+++ src/main/java/frc/robot/commands/DriveWithApriltag.java
@@ -59,8 +59,13 @@ public class DriveWithApriltag extends Command {
         // calculate error
         double xError = offsetArray[0] - drivePosition.getX(); // meters
         double yError = offsetArray[1] - drivePosition.getY() - 0.14 * (left ? -1 : 1); // meters
+        //double yError = offsetArray[1] - drivePosition.getY(); // meters //!  for only middle tracking
         double oOffset = PhysicalConstants.APRILTAG_LOCATIONS.get(targetTagId).getRotation().getRadians() - Math.PI; // radians
         double oError = oOffset - MathUtil.angleModulus(poseEstimator.getRawYaw().getRadians()); // radians
+        //double oError = Math.toRadians(25) * (left ? -1 : 1); //! middle tracking (25 degress is wrong)
+        // double xKp = 1; //! these  p's are for pid version
+        // double yKp = 1;
+        // double oKp = 1;
         // ! but what if gyro is pi and target is -pi
 
         Logger.recordOutput("oOffset", oOffset);
@@ -107,6 +112,94 @@ public class DriveWithApriltag extends Command {
                 break;
         }
 
+
+        // ! pid version
+        // ChassisSpeeds speeds = new ChassisSpeeds();
+        // switch (driveMode) {
+        //     case ROTATE: 
+        //         speeds = new ChassisSpeeds(
+        //             0,
+        //             0,
+        //             Math.abs(oError) > 0.001 ? oError * oKp : 0
+        //         );
+        //         if (speeds.omegaRadiansPerSecond == 0) {
+        //             driveMode = DriveMode.Y;
+        //         }
+        //         break;
+        //     case Y: 
+        //         speeds = new ChassisSpeeds(
+        //             0,
+        //             Math.abs(yError) > 0.001 ? yError * yKp : 0,
+        //             0
+        //         );
+        //         if (speeds.vyMetersPerSecond == 0) {
+        //             driveMode = DriveMode.X;
+        //             return;
+        //         }
+        //         break;
+        //     case X: 
+        //         speeds = new ChassisSpeeds(
+        //             Math.abs(xError) > 0.001 ? xError * xKp: 0,
+        //             0,
+        //             0
+        //         );
+        //         if (speeds.vxMetersPerSecond == 0) {
+        //             isFinished = true;
+        //             return; 
+        //         }
+        //         break;
+        // }
+
+
+
+
+
+
+
+            //! home to middle then turn # radians (need to make arm raised a bit to use his)
+
+        // ChassisSpeeds speeds = new ChassisSpeeds();
+        // switch (driveMode) {
+        //     case ROTATE: 
+        //         speeds = new ChassisSpeeds(
+        //             0,
+        //             0,
+        //             Math.abs(oError) > 0.001 ? Math.signum(oError) * 0.3 : 0
+        //         );
+        //         if (speeds.omegaRadiansPerSecond == 0) {
+        //             driveMode = DriveMode.Y;
+        //         }
+        //         break;
+        //     case Y: 
+        //         speeds = new ChassisSpeeds(
+        //             0,
+        //             Math.abs(yError) > 0.001 ? Math.signum(yError) * 0.1 : 0,
+        //             0
+        //         );
+        //         if (speeds.vyMetersPerSecond == 0) {
+        //             driveMode = DriveMode.X;
+        //             return;
+        //         }
+        //         break;
+        //     case X: 
+        //         speeds = new ChassisSpeeds(
+        //             Math.abs(xError) > 0.001 ? Math.signum(xError) * 0.1 : 0,
+        //             0,
+        //             0
+        //         );
+        //         if (speeds.vxMetersPerSecond == 0) {
+        //             isFinished = true;
+        //             return; 
+        //         }
+        //         break;
+        // }
+
+
+
+
+        
+
+
         speeds = new ChassisSpeeds(
             Math.abs(xError) > 0.005 ? Math.signum(xError) * 0.2 : 0,
             Math.abs(yError) > 0.005 ? Math.signum(yError) * 0.2 : 0,
@@ -115,7 +208,7 @@ public class DriveWithApriltag extends Command {
 
         if (speeds.vxMetersPerSecond == 0
         &&  speeds.vyMetersPerSecond == 0
-        &&  speeds.omegaRadiansPerSecond == 0
+        &&  speeds.omegaRadiansPerSecond == 0 
         ) {
             isFinished = true;
             return; 
