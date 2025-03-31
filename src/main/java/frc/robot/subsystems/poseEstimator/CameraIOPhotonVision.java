37dc34dabd9d38d6448305ae06c2bbb440379bda
willmell <whickey27@concordcarlisle.org>
2025-03-31 19:43:56 -0400

    whyyyy

--- src/main/java/frc/robot/subsystems/poseEstimator/CameraIOPhotonVision.java
+++ src/main/java/frc/robot/subsystems/poseEstimator/CameraIOPhotonVision.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.poseEstimator;
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.littletonrobotics.junction.Logger;
@@ -27,57 +29,58 @@ public class CameraIOPhotonVision {
     PhotonPoseEstimator PoseEstimator;
     String CameraName;
     Optional<EstimatedRobotPose> EstimatedRobotPose;
-    PhotonPipelineResult EstimatorResult;
+    List<PhotonPipelineResult> EstimatorResult;
     String cameraPrefix;
-    
 
-    
     public CameraIOPhotonVision(
-        PhotonCamera camera,
-        String CameraName,
-        Transform3d CameraTransform
-    ){
+            PhotonCamera camera,
+            String CameraName,
+            Transform3d CameraTransform) {
         this.camera = camera;
-        
         this.CameraName = CameraName;
-        this.EstimatedRobotPose =  Optional.empty();
-        this.EstimatorResult = new PhotonPipelineResult();
-        this.PoseEstimator = new PhotonPoseEstimator(PhysicalConstants.TagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, new Transform3d());
+        this.EstimatedRobotPose = Optional.empty();
+        this.PoseEstimator = new PhotonPoseEstimator(PhysicalConstants.TagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+                new Transform3d());
         PoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
         cameraPrefix = "Vision/" + CameraName + "/";
         System.out.println(CameraName + " Camera Innitilized");
-        
-        
-
+        EstimatorResult = new ArrayList<>(); 
+        EstimatorResult.add(new PhotonPipelineResult());
     }
 
 
-
     public void periodic(){
-        EstimatorResult = camera.getLatestResult();
-        System.out.println(EstimatorResult);
-        EstimatedRobotPose = PoseEstimator.update(EstimatorResult);
-        ArrayList<PhotonTrackedTarget> Tags = new ArrayList<PhotonTrackedTarget>();
-        Pose3d Pose = UpdateVision();
+        EstimatorResult.clear();
+        EstimatorResult = camera.getAllUnreadResults();
+        if(EstimatorResult.size() > 0){
+            EstimatedRobotPose = PoseEstimator.update(EstimatorResult.get(0));
+            ArrayList<PhotonTrackedTarget> Tags = new ArrayList<PhotonTrackedTarget>();
+            Pose3d Pose = UpdateVision();
+    
 
-        for (PhotonTrackedTarget tag : EstimatorResult.getTargets()) {
-            Tags.add(tag);
+
+            for (PhotonTrackedTarget tag : EstimatorResult.get(0).getTargets()) {
+                Tags.add(tag);
+            }
+
+
+            Logger.recordOutput(cameraPrefix + "connected", camera.isConnected());
+            Logger.recordOutput(cameraPrefix + "timestamp", EstimatorResult.get(0).getTimestampSeconds());
+                
+            if (Pose != null) {
+                System.out.println("NOT NULLLLLLLL" + Pose);
+                Logger.recordOutput(cameraPrefix + "VisionPose2d", Pose.toPose2d());
+                Logger.recordOutput(cameraPrefix + "VisionPose3d", Pose);
+            } else {
+                System.out.println("NULLLLLLLL");
+                Logger.recordOutput(cameraPrefix + "VisionPose2d", "null");
+                Logger.recordOutput(cameraPrefix + "VisionPose3d", "null");
+            }
+                
+
+            Logger.recordOutput(cameraPrefix + "NumTags", Tags.size());
+
         }
-
-
-        Logger.recordOutput(cameraPrefix + "connected", camera.isConnected());
-        Logger.recordOutput(cameraPrefix + "timestamp", EstimatorResult.getTimestampSeconds());
-            
-        if (Pose != null) {
-            Logger.recordOutput(cameraPrefix + "VisionPose2d", Pose.toPose2d());
-            Logger.recordOutput(cameraPrefix + "VisionPose3d", Pose);
-        } else {
-            Logger.recordOutput(cameraPrefix + "VisionPose2d", "null");
-            Logger.recordOutput(cameraPrefix + "VisionPose3d", "null");
-        }
-            
-
-        Logger.recordOutput(cameraPrefix + "NumTags", Tags.size());
 
 
     }
@@ -98,7 +101,7 @@ public class CameraIOPhotonVision {
             System.out.println("Not in field");
             return null;
         }
-
+        
         return pose;
     }
 
