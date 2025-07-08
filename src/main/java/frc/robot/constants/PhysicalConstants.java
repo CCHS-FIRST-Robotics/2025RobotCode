d7f77d2bbe9a3e6aaea650e922779326a2c9f757
AlextheChild <alexjun2008@gmail.com>
2025-07-08 18:54:52 -0400

    beep boop

--- src/main/java/frc/robot/constants/PhysicalConstants.java
+++ src/main/java/frc/robot/constants/PhysicalConstants.java
@@ -12,7 +12,6 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.*;
-import java.util.*;
 
 public final class PhysicalConstants {
 
@@ -107,40 +106,6 @@ public final class PhysicalConstants {
     public static final int TOO_FAR_AWAY = 6; // meters at which tag distances to pose are thrown out
 
     public static final AprilTagFieldLayout APRILTAG_LAYOUT = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
-    public static final HashMap<Integer, Pose2d> APRILTAG_LOCATIONS = new HashMap<Integer, Pose2d>();
-    static {
-        APRILTAG_LOCATIONS.put(1, new Pose2d(16.697198, 0.65532, new Rotation2d(Degrees.of(126))));
-        APRILTAG_LOCATIONS.put(2, new Pose2d(16.697198, 7.39648, new Rotation2d(Degrees.of(234))));
-        APRILTAG_LOCATIONS.put(3, new Pose2d(11.56081, 8.05561, new Rotation2d(Degrees.of(270))));
-        APRILTAG_LOCATIONS.put(4, new Pose2d(9.27608, 6.137656, new Rotation2d(Degrees.of(0))));
-        APRILTAG_LOCATIONS.put(5, new Pose2d(9.27608, 1.914906, new Rotation2d(Degrees.of(0))));
-        APRILTAG_LOCATIONS.put(6, new Pose2d(13.474446, 3.306318, new Rotation2d(Degrees.of(300))));
-        APRILTAG_LOCATIONS.put(7, new Pose2d(13.890498, 4.0259, new Rotation2d(Degrees.of(0))));
-        APRILTAG_LOCATIONS.put(8, new Pose2d(13.474446, 4.745482, new Rotation2d(Degrees.of(60))));
-        APRILTAG_LOCATIONS.put(9, new Pose2d(12.643358, 4.745482, new Rotation2d(Degrees.of(120))));
-        APRILTAG_LOCATIONS.put(10, new Pose2d(12.227306, 4.0259, new Rotation2d(Degrees.of(180))));
-        APRILTAG_LOCATIONS.put(11, new Pose2d(12.643358, 3.306318, new Rotation2d(Degrees.of(240))));
-        APRILTAG_LOCATIONS.put(12, new Pose2d(0.851154, 0.65532, new Rotation2d(Degrees.of(54))));
-        APRILTAG_LOCATIONS.put(13, new Pose2d(0.851154, 7.39648, new Rotation2d(Degrees.of(306))));
-        APRILTAG_LOCATIONS.put(14, new Pose2d(8.272272, 6.137656, new Rotation2d(Degrees.of(180))));
-        APRILTAG_LOCATIONS.put(15, new Pose2d(8.272272, 1.914906, new Rotation2d(Degrees.of(180))));
-        APRILTAG_LOCATIONS.put(16, new Pose2d(5.987542, -0.00381, new Rotation2d(Degrees.of(90))));
-        APRILTAG_LOCATIONS.put(17, new Pose2d(4.073906, 3.306318, new Rotation2d(Degrees.of(240))));
-        APRILTAG_LOCATIONS.put(18, new Pose2d(3.6576, 4.0259, new Rotation2d(Degrees.of(180))));
-        APRILTAG_LOCATIONS.put(19, new Pose2d(4.073906, 4.745482, new Rotation2d(Degrees.of(120))));
-        APRILTAG_LOCATIONS.put(20, new Pose2d(4.90474, 4.745482, new Rotation2d(Degrees.of(60))));
-        APRILTAG_LOCATIONS.put(21, new Pose2d(5.321046, 4.0259, new Rotation2d(Degrees.of(0))));
-        APRILTAG_LOCATIONS.put(22, new Pose2d(4.90474, 3.306318, new Rotation2d(Degrees.of(300))));
-    }
-
-    public static final class DrivePositions {
-        public static final Translation2d INTAKE = new Translation2d(0, 0); // ! no idea how this would work
-        public static final Translation2d L1 = new Translation2d(0, 0);
-        public static final Translation2d L2 = new Translation2d(0, 0); // ! 
-        public static final Translation2d L3 = new Translation2d(0, 0); // ! 
-        public static final Translation2d L4 = new Translation2d(0.63, 0);
-
-    }
 
     // ————— coral constants ————— //
 
